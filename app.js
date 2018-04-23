bcd29183058c989baca2f1b2ffa1d2f533cbf1d5
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-04-23 17:45:07 +0100

    Adding video tutorial to readme

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 comst input = document.querySelector('#searchTerm');
 cost API_URL = 'https://omdb-api.now.sh/t?ype=movie&s=';
+const resultsSection = document.querySelector('#results')
 
 form.addEventListener('submit', formSubmitted);
 
@@ -15,4 +16,22 @@ function formSubmitted(event){
 function getResults(searchTerm){
     const url = `${API_URL}${searchTerm}`;
     console.log(url);
+    fetch(url)
+    .then(response => response.json())
+   // .then(data =>
+     //   {
+           // console.log(data)
+     //   })
+    .then(data => showResults(data.Search));
+}
+
+function showResults(results){
+    resultsSection.innerHTML = '';
+    const html = '';
+    //console.log(results)
+    results.forEach(movie =>{
+       // console.log(movie)
+       html +=`<h3>${movie.Title}</h3>`;
+    })
+    resultsSection.innerHTML = html;
 }
