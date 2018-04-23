e64c8529f043af37f38eb64dd6a7d898c1d1fc27
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-04-23 17:27:56 +0100

    Adding API Call functionality

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const form = document.querySelector('form');
+comst input = document.querySelector('#searchTerm');
+cost API_URL = 'https://omdb-api.now.sh/t?ype=movie&s=';
 
 form.addEventListener('submit', formSubmitted);
 
 function formSubmitted(event){
     console.log('form submitted');
     event.preventDefault();
+    const searchTerm = input.value;
+    console.log(searchTerm);
+    getResult(searchTerm);
+}
+
+function getResults(searchTerm){
+    const url = `${API_URL}${searchTerm}`;
+    console.log(url);
 }
