5176254e88f00604945379eb333642cc4e65bf07
Abigail Travers <abigailtravers@PEG-MAC-007281.local>
2018-04-23 17:49:01 +0100

    adding a card

--- app.js
+++ app.js
@@ -31,7 +31,18 @@ function showResults(results){
     //console.log(results)
     results.forEach(movie =>{
        // console.log(movie)
-       html +=`<h3>${movie.Title}</h3>`;
+       html +=
+       <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+       
+       
+      // `<h3>${movie.Title}</h3>`;
     })
     resultsSection.innerHTML = html;
 }
